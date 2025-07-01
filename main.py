91b1040eaf8905df6e44a5a555dc35bdb1abc76c
Krilith <jasonvank@yahoo.com>
2025-07-01 17:06:57 -0400

    2.8 put player into groups

--- main.py
+++ main.py
@@ -1,50 +1,58 @@
-###imports
+# #imports
 import pygame
 from constants import *
 from player import *
 
-##Main game 
+# Main game 
 def main():
-    ## initilize imported pygame modules
+    #  initilize imported pygame modules
     pygame.init()
 
-    ##create game clock
+    # create game clock
     clock = pygame.time.Clock()
     dt = 0
 
-    ##Create player
+    # Create groups
+    updatable = pygame.sprite.Group()
+    drawable = my_group = pygame.sprite.Group()
+
+    # Add to groups
+    Player.containers = (updatable, drawable)
+
+    # Create player
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
-    ## console print stuff at launch
+    #  console print stuff at launch
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    ##start gui
+    # start gui
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    ##start of loop
+    # start of loop
     while True:
-        ##Makes windows close button work
+        # Makes windows close button work
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        ## set screen to black
+        #  set screen to black
         screen.fill(000000)
 
-        ##rotate player
-        player.update(dt)
+        # rotate player +
+        updatable.update(dt)
 
-        ##draw player
-        player.draw(screen)
+        # draw player +
+        for each in drawable:
+            each.draw(screen)
 
-        ##update screen
+        # update screen
         pygame.display.flip()
 
         #update gametime
         dt = clock.tick(60) / 1000
                 
-##Start main if executed, not imported
+# Start main if executed, not imported
 if __name__ == "__main__":
     main()
