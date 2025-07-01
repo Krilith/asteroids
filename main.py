b01e06c8a5fc73c6c84394e5814d5886be0dbef8
Krilith <jasonvank@yahoo.com>
2025-07-01 18:18:30 -0400

    3.3 Added shot to players.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from asteroidfield import *
 
 # Main game 
 def main():
-    #  initilize imported pygame modules
+    # initilize imported pygame modules
     pygame.init()
 
     # create game clock
@@ -18,11 +18,13 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     # Add to groups
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
 
     # Create player
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
--- player.py
+++ player.py
@@ -3,6 +3,7 @@ from circleshape import *
 from constants import *
 
 class Player(CircleShape):
+    
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
@@ -33,7 +34,25 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(dt * -1)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
     
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+    def shoot(self):
+        shot = Shot(self.position, self.rotation)
+    
+
+class Shot(CircleShape):
+
+    def __init__(self, player_position,player_rotation):
+        super().__init__(player_position[0], player_position[1], SHOT_RADIUS)
+        self.velocity = pygame.Vector2(0,1).rotate(player_rotation)
+
+    def draw(self, screen):
+        white = (255, 255, 255)
+        pygame.draw.circle(screen, white, (self.position), SHOT_RADIUS, 2)
+
+    def update(self, dt):
+            self.position += self.velocity * dt * PLAYER_SHOOT_SPEED
