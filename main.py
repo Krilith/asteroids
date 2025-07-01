63860eac2e2a84be98fee82917a4cd23dcae6fde
Krilith <jasonvank@yahoo.com>
2025-07-01 14:47:08 -0400

    2.3 added game clock

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def main():
     ## initilize imported pygame modules
     pygame.init()
 
+    ##create game clock
+    clock = pygame.time.Clock()
+    dt = 0
+
     ## console print stuff at launch
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -23,10 +27,15 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
+        ## set screen to black
         screen.fill(000000)
 
+        ##update screen
         pygame.display.flip()
 
+        #update gametime
+        dt = clock.tick(60) / 1000
+                
 ##Start main if executed, not imported
 if __name__ == "__main__":
     main()
